726865a746a7200f6aab32efec7cc6d856a4e21f
Omer <omer-meraloglu@hotmail.com>
2022-11-29 22:04:15 +0300

    created movie component for more clean code

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,9 +1,11 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import Movie from './Movie';
 
 const Row = ({title, fetchURL}) => {
    
     const[movies,setMovies] = useState([])
+    
 
     useEffect(() => {
         async function fetchData() {
@@ -20,11 +22,9 @@ const Row = ({title, fetchURL}) => {
     <>
         <h2 className='text-white font-bold md:text-xl p-4'>{title}</h2>
         <div className='relative flex items-center'>
-            <div id={'slider'}>
+            <div id={'slider'} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'>
             {movies.map((item, id) => (
-                    <div className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2 '>
-                        <img src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`} alt={item.title} />
-                    </div>
+                    <Movie key={id} item={item}/>
                 ))}
             </div>
         </div>
