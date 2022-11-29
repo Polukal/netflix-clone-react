24c1f500c36da116f9f7a28d015f942ee49d9a88
Omer <omer-meraloglu@hotmail.com>
2022-11-29 16:30:26 +0300

    fixed response issue

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -5,12 +5,15 @@ const Row = ({title, fetchURL}) => {
    
     const[movies,setMovies] = useState([])
 
-    useEffect(()=>{
-         axios.get(fetchURL).then((response) => {
-            setMovies(response.data.result)
-        })
-    },[fetchURL])
-
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(fetchURL)
+            setMovies(request.data.results)
+            return request
+        }
+        fetchData()
+    }
+    , [fetchURL])
     console.log(movies)
 
   return (
@@ -18,7 +21,11 @@ const Row = ({title, fetchURL}) => {
         <h2 className='text-white font-bold md:text-xl p-4'>{title}</h2>
         <div className='relative flex items-center'>
             <div id={'slider'}>
-                
+            {movies.map((item, id) => (
+                    <div className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2 '>
+                        <img src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`} alt={item.title} />
+                    </div>
+                ))}
             </div>
         </div>
 
@@ -34,4 +41,15 @@ export default Row
                         <img src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`} alt={item.title} />
                     </div>
                 ))}
+
+                useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(fetchURL)
+            setMovies(request.data.results)
+            return request
+        }
+        fetchData()
+    }
+    , [fetchURL])
+    console.log(movies)
 */
